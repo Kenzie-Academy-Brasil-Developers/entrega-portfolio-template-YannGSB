0cadad99df59b79fbd9f12151afd968df1a675a7
Yann G. Simonetti Buffet <yanngsb@hotmail.com>
2023-09-25 01:46:06 -0300

    fix: projects session style fix

--- src/components/ProjectsSection/ProjectCard/index.jsx
+++ src/components/ProjectsSection/ProjectCard/index.jsx
@@ -1,11 +1,12 @@
 import gitHubIcon from "../../../assets/git-icon.png";
+import styles from "./style.module.css"
 
 export const ProjectCard = ({ name, description }) => {
   return (
     <>
-      <div>
+      <div className={styles.listItemContainer}>
         <h3 className="title3">{name}</h3>
-        <p className="paragraph">{description}</p>
+        <p className={`paragraph ${styles.itemParagraph}`}>{description}</p>
         <a className="link" href="#">
           Saiba mais
         </a>
--- src/components/ProjectsSection/index.jsx
+++ src/components/ProjectsSection/index.jsx
@@ -1,14 +1,15 @@
 import { ProjectCard } from "./ProjectCard";
 import { projects } from "../../data/projects";
+import styles from "./style.module.css";
 
 export const ProjectSection = () => {
   return (
     <section>
       <h2 className="title2">Projetos</h2>
-      <ul>
+      <ul className={styles.listContainer}>
         {projects.map((project) => {
           return (
-            <li>
+            <li className={styles.listItem}>
               <ProjectCard
                 description={project.description}
                 name={project.name}
